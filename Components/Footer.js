1aa67e0503e93c8342ab7ac102aeca9072305c7d
Arsath8597 <111648806+Arsath8597@users.noreply.github.com>
2025-01-18 21:36:48 +0530

    Add files via upload

---
+++ Components/Footer.js
@@ -0,0 +1,197 @@
+import React from "react";
+import img1 from "../images/hhmalogo (1).jpg";
+import { AiFillFacebook } from "react-icons/ai";
+import { AiOutlinePhone } from "react-icons/ai";
+import { BiCurrentLocation } from "react-icons/bi";
+import { AiOutlineWhatsApp } from "react-icons/ai";
+import { BiLogoInstagram } from "react-icons/bi";
+import { BiLogoGmail } from "react-icons/bi";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="relative z-10 bg-black text-white pt-20 pb-10 lg:pt-[120px] lg:pb-20">
+        <div className="container">
+          <div className="flex flex-wrap -mx-4">
+            <div className="w-full px-4 sm:w-2/3 lg:w-3/12">
+              <div className="w-full mb-10">
+                <a href="/sample" className="mb-6 inline-block max-w-[160px]">
+                  <img src={img1} alt="logo" className="max-w-full" />
+                </a>
+                <p className="text-base mb-7 text-body-color">
+                  Sed ut perspiciatis undmnis is iste natus error sit amet
+                  voluptatem totam rem aperiam.
+                </p>
+                <p className="flex text-xl items-center mt-5  font-medium text-dark">
+                  <span className="mr-3 text-primary">
+                    <AiOutlinePhone />
+                  </span>
+                  <a href="tel:+94767313385">+94767313385</a>
+                </p>
+                <p className="flex items-center text-xl mt-5 font-medium text-dark">
+                  <span className="mr-3 text-primary">
+                    <BiLogoGmail />
+                  </span>
+                  <a href="mailto:info@hhmasrilanka.org">
+                    info@hhmasrilanka.org
+                  </a>
+                </p>
+                <p className="flex items-center text-xl mt-5 font-medium text-dark">
+                  <span className="mr-3 text-primary">
+                    <AiOutlineWhatsApp />
+                  </span>
+                  <a href="https://wa.me/+94767313385">+94767313385</a>
+                </p>
+              </div>
+            </div>
+
+            <LinkGroup header="Quick Links">
+              <NavLink link="/#" label="Premium Support" />
+              <NavLink link="/#" label="Our Services" />
+              <NavLink link="/#" label="Know Our Team" />
+              <NavLink link="/#" label="Download App" />
+            </LinkGroup>
+
+            <div className="w-full px-4 sm:w-1/2 lg:w-3/12">
+              <div className="w-full mb-10">
+                <h4 className="text-lg font-semibold mb-9 text-dark">
+                  Follow us On
+                </h4>
+                <div className="flex items-center mb-6">
+                  <a
+                    href="https://www.facebook.com/profile.php?id=100083648684170&mibextid=ZbWKwL"
+                    className="mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-[#E5E5E5] text-dark hover:border-primary hover:bg-primary hover:text-white sm:mr-4 lg:mr-3 xl:mr-4"
+                  >
+                    <svg
+                      width={8}
+                      height={16}
+                      viewBox="0 0 8 16"
+                      className="fill-current"
+                    >
+                      <path d="M7.43902 6.4H6.19918H5.75639V5.88387V4.28387V3.76774H6.19918H7.12906C7.3726 3.76774 7.57186 3.56129 7.57186 3.25161V0.516129C7.57186 0.232258 7.39474 0 7.12906 0H5.51285C3.76379 0 2.54609 1.44516 2.54609 3.5871V5.83226V6.34839H2.10329H0.597778C0.287819 6.34839 0 6.63226 0 7.04516V8.90323C0 9.26452 0.243539 9.6 0.597778 9.6H2.05902H2.50181V10.1161V15.3032C2.50181 15.6645 2.74535 16 3.09959 16H5.18075C5.31359 16 5.42429 15.9226 5.51285 15.8194C5.60141 15.7161 5.66783 15.5355 5.66783 15.3806V10.1419V9.62581H6.13276H7.12906C7.41688 9.62581 7.63828 9.41935 7.68256 9.10968V9.08387V9.05806L7.99252 7.27742C8.01466 7.09677 7.99252 6.89032 7.85968 6.68387C7.8154 6.55484 7.61614 6.42581 7.43902 6.4Z" />
+                    </svg>
+                  </a>
+                  <a
+                    href="https://twitter.com/hhma_sl"
+                    className="mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-[#E5E5E5] text-dark hover:border-primary hover:bg-primary hover:text-white sm:mr-4 lg:mr-3 xl:mr-4"
+                  >
+                    <svg
+                      width={16}
+                      height={12}
+                      viewBox="0 0 16 12"
+                      className="fill-current"
+                    >
+                      <path d="M14.2194 2.06654L15.2 0.939335C15.4839 0.634051 15.5613 0.399217 15.5871 0.2818C14.8129 0.704501 14.0903 0.845401 13.6258 0.845401H13.4452L13.3419 0.751468C12.7226 0.258317 11.9484 0 11.1226 0C9.31613 0 7.89677 1.36204 7.89677 2.93542C7.89677 3.02935 7.89677 3.17025 7.92258 3.26419L8 3.73386L7.45806 3.71037C4.15484 3.61644 1.44516 1.03327 1.00645 0.587084C0.283871 1.76125 0.696774 2.88845 1.13548 3.59296L2.0129 4.90802L0.619355 4.20352C0.645161 5.18982 1.05806 5.96477 1.85806 6.52838L2.55484 6.99804L1.85806 7.25636C2.29677 8.45401 3.27742 8.94716 4 9.13503L4.95484 9.36986L4.05161 9.93346C2.60645 10.8728 0.8 10.8024 0 10.7319C1.62581 11.7652 3.56129 12 4.90323 12C5.90968 12 6.65806 11.9061 6.83871 11.8356C14.0645 10.2857 14.4 4.41487 14.4 3.2407V3.07632L14.5548 2.98239C15.4323 2.23092 15.7935 1.8317 16 1.59687C15.9226 1.62035 15.8194 1.66732 15.7161 1.6908L14.2194 2.06654Z" />
+                    </svg>
+                  </a>
+                  <a
+                    href="https://wa.me/+94767313385"
+                    className="mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-[#E5E5E5] text-dark hover:border-primary hover:bg-primary hover:text-white sm:mr-4 lg:mr-3 xl:mr-4"
+                  >
+                    <AiOutlineWhatsApp />
+                  </a>
+                  <a
+                    href="https://instagram.com/hhmasrilanka"
+                    className="mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-[#E5E5E5] text-dark hover:border-primary hover:bg-primary hover:text-white sm:mr-4 lg:mr-3 xl:mr-4"
+                  >
+                    <BiLogoInstagram />
+                  </a>
+                </div>
+                <p className="text-base text-body-color">
+                  {" "}
+                  © {new Date().getFullYear()} TailGrids{" "}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <span className="absolute left-0 bottom-0 z-[-1]">
+            <svg
+              width={217}
+              height={229}
+              viewBox="0 0 217 229"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M-64 140.5C-64 62.904 -1.096 1.90666e-05 76.5 1.22829e-05C154.096 5.49924e-06 217 62.904 217 140.5C217 218.096 154.096 281 76.5 281C-1.09598 281 -64 218.096 -64 140.5Z"
+                fill="url(#paint0_linear_1179_5)"
+              />
+              <defs>
+                <linearGradient
+                  id="paint0_linear_1179_5"
+                  x1="76.5"
+                  y1={281}
+                  x2="76.5"
+                  y2="1.22829e-05"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#3056D3" stopOpacity="0.08" />
+                  <stop offset={1} stopColor="#C4C4C4" stopOpacity={0} />
+                </linearGradient>
+              </defs>
+            </svg>
+          </span>
+          <span className="absolute top-10 right-10 z-[-1]">
+            <svg
+              width={75}
+              height={75}
+              viewBox="0 0 75 75"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M37.5 -1.63918e-06C58.2107 -2.54447e-06 75 16.7893 75 37.5C75 58.2107 58.2107 75 37.5 75C16.7893 75 -7.33885e-07 58.2107 -1.63918e-06 37.5C-2.54447e-06 16.7893 16.7893 -7.33885e-07 37.5 -1.63918e-06Z"
+                fill="url(#paint0_linear_1179_4)"
+              />
+              <defs>
+                <linearGradient
+                  id="paint0_linear_1179_4"
+                  x1="-1.63917e-06"
+                  y1="37.5"
+                  x2={75}
+                  y2="37.5"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#13C296" stopOpacity="0.31" />
+                  <stop offset={1} stopColor="#C4C4C4" stopOpacity={0} />
+                </linearGradient>
+              </defs>
+            </svg>
+          </span>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
+
+const LinkGroup = ({ children, header }) => {
+  return (
+    <>
+      <div className="w-full px-4 sm:w-1/2 lg:w-2/12">
+        <div className="w-full mb-10">
+          <h4 className="text-lg font-semibold mb-9 text-dark"> {header} </h4>
+          <ul>{children}</ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const NavLink = ({ link, label }) => {
+  return (
+    <div>
+      <li>
+        <a
+          href={link}
+          className={`inline-block mb-2 text-base leading-loose text-body-color hover:text-primary`}
+        >
+          {label}
+        </a>
+      </li>
+    </div>
+  );
+};
---
+++ Components/Pages/Qurbani.js
@@ -0,0 +1,248 @@
+import React from 'react';
+import { TERipple } from 'tw-elements-react';
+import img1 from '../../images/WhatsApp Image 2024-06-30 at 11.34.37 AM.jpeg'
+import img2 from '../../images/WhatsApp Image 2024-06-30 at 11.34.38 AM.jpeg'
+import img3 from '../../images/WhatsApp Image 2024-06-30 at 11.34.44 AM.jpeg'
+import img4 from '../../images/WhatsApp Image 2024-06-30 at 11.34.44 AM.jpeg'
+import img5 from '../../images/WhatsApp Image 2024-06-30 at 11.34.44 AM (1).jpeg'
+import img6 from '../../images/WhatsApp Image 2024-06-30 at 11.34.44 AM (2).jpeg'
+import img7 from '../../images/WhatsApp Image 2024-06-30 at 11.34.45 AM.jpeg'
+import img8 from '../../images/WhatsApp Image 2024-06-30 at 11.34.45 AM (1).jpeg'
+import img9 from '../../images/WhatsApp Image 2024-06-30 at 11.34.47 AM.jpeg'
+import img10 from '../../images/WhatsApp Image 2024-06-30 at 11.34.47 AM (1).jpeg'
+import img11 from '../../images/WhatsApp Image 2024-06-30 at 11.34.48 AM.jpeg'
+import img12 from '../../images/WhatsApp Image 2024-06-30 at 11.34.48 AM (1).jpeg'
+import img13 from '../../images/WhatsApp Image 2024-06-30 at 11.34.49 AM (1).jpeg'
+import img14 from '../../images/WhatsApp Image 2024-06-30 at 11.34.49 AM (2).jpeg'
+import img15 from '../../images/WhatsApp Image 2024-06-30 at 11.34.49 AM (3).jpeg'
+import img16 from '../../images/WhatsApp Image 2024-06-30 at 11.34.50 AM.jpeg'
+import img17 from '../../images/WhatsApp Image 2024-06-30 at 11.35.10 AM.jpeg'
+import img18 from '../../images/WhatsApp Image 2024-06-30 at 11.35.11 AM.jpeg'
+import img19 from '../../images/WhatsApp Image 2024-06-30 at 11.35.11 AM (1).jpeg'
+import img20 from '../../images/WhatsApp Image 2024-06-30 at 11.35.12 AM.jpeg'
+
+import img21 from "../../images/WhatsApp Image 2024-06-30 at 11.35.12 AM (1).jpeg";
+import img22 from "../../images/WhatsApp Image 2024-06-30 at 11.35.13 AM.jpeg";
+import img23 from "../../images/WhatsApp Image 2024-06-30 at 11.35.13 AM (1).jpeg";
+import img24 from "../../images/WhatsApp Image 2024-06-30 at 11.35.14 AM.jpeg";
+import img25 from "../../images/WhatsApp Image 2024-06-30 at 11.35.14 AM (1).jpeg";
+import img26 from "../../images/WhatsApp Image 2024-06-30 at 11.35.15 AM.jpeg";
+import img27 from "../../images/WhatsApp Image 2024-06-30 at 11.35.16 AM.jpeg";
+import img28 from "../../images/WhatsApp Image 2024-06-30 at 11.35.16 AM (1).jpeg";
+import img29 from "../../images/WhatsApp Image 2024-06-30 at 11.35.16 AM (2).jpeg";
+import img30 from "../../images/WhatsApp Image 2024-06-30 at 11.35.17 AM.jpeg";
+
+const Qurbani = () => {
+  return (
+      <div
+        className=" rounded-lg xl:px-28 mt-28  bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]">
+        <h3
+            className="mb-2 text-center text-2xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+          Qurbani - 2024 - Sri Lanka 
+          </h3>
+        
+        <div className="p-6">
+          
+          <p className="my-7 font-bold text-xl  text-black">
+          We shared our joys on Eid al-Adha.😊 Your small donation make big difference 🎉🎉🎉</p>
+          <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img1}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img2}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img3}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img4}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img5}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img6}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img7}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img8}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img9}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img10}
+            alt="" />
+        </a>
+        
+       
+          <p className='my-8'>
+          Alhamdulillah 🤲
+Our Qurban Project is going good. We thank Allah (swt) for allowing us to perform Qurban sacrifice on your behalf.
+         
+          </p>
+          <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img11}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img12}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img13}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img14}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img15}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img16}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img17}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img18}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img19}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img20}
+            alt="" />
+        </a>
+          <p className='my-7'> 
+          ALLAH SAYS IN QURAN
+“Therefore turn in prayer to your Lord and sacrifice (to Him only).” [al-Kawthar 108:2]
+The poor, the destitute, the orphans, the widowed mothers, the disabled and the special need families are fed and happy through your Qurbani meats.
+          </p>
+          <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img21}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img22}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img23}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img24}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img25}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img26}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img27}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img28}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img29}
+            alt="" />
+        </a>
+        <a href="#!">
+          <img
+            className="rounded-t-lg py-1"
+            src={img30}
+            alt="" />
+        </a>
+<p className='my-6 '> 
+Thousands around the Sri Lankan are suffering and are unable to provide meat for their families during Eid Al Adha! Share your Qurban and be the reason for smiles and happiness during their Eid Celebrations🤲
+</p>
+
+        </div>
+      </div>
+  );
+}
+export default Qurbani
